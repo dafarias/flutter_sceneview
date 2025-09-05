38db4aad47b9669bcdcb236d5e95206529df9ce9
Adquinson Farias <adamianfm@gmail.com>
2025-09-05 15:13:45 +0200

    Add scene channel snapshot method.

--- lib/src/channels/scene_channel.dart
+++ lib/src/channels/scene_channel.dart
@@ -1,9 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 class SceneChannel {
   final MethodChannel _channel;
 
   SceneChannel(this._channel);
+
+  Future<Uint8List> sceneSnapshot() async {
+    final imageBytes = await _channel.invokeMethod<Uint8List>('takeSnapshot');
+    return imageBytes ?? Uint8List.fromList([]);
+  }
+
+  Future<void> dispose() async {
+    try {
+      await _channel.invokeMethod('dispose', "sceneId");
+    } catch (e) {
+      debugPrint('SceneChannel dispose error: $e');
+    }
+  }
 }
-
-
